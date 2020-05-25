a603b8ad362b403fbeb268f29310c0a03f7ae121
Enes Akıllıok <46111016+yourpalenes@users.noreply.github.com>
2020-05-25 16:34:41 +0300

    fix .map download

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ function updateResource()
         for index, node in ipairs(metaData) do
             local fileType = xmlNodeGetName(node)
             local fileLocation = xmlNodeGetAttribute(node, "src")
-            if fileType == "script" or fileType == "file" then
+            if fileType == "script" or fileType == "file" or fileType == "config" or fileType == "map" or fileType == "html" then
                 resourceFileCache[#resourceFileCache + 1] = fileLocation
             end
         end
